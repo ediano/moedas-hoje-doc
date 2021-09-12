0a7b984b6dd7880ccc25ba0e0a560976daf6a9fc
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2021-09-12 17:21:22 -0400

    add pages tickers and ticker

---
+++ content/pages/tickers.md
@@ -0,0 +1,24 @@
+---
+title: Tickers
+versions: 'v1'
+patchUrl: 'coins/tickers'
+source: 'source=novadax.com'
+asset: 'asset=traditional'
+---
+
+Listando pares de moedas. Um de nossos principais recursos é listar todos os pares de moedas e exchanges de uma unica vez.
+
+Entretanto também é possivel litar todos os pares de moedas de uma unica exchanges ou por exchanges baseadas no tipos de ativos suportados por elas, como moedas tradicionais (governamentais) e ou criptomoedas (ativos digitais).
+
+## Listar por exchange
+
+Para litas todos os pares de moedas por exchange o seguinte parametro é esperado `source` seguido do seu valor `dominio da exchange`.
+
+> Exemplo: `source=novadax.com`
+
+
+## Listar por tipo de ativo
+
+Por outro lado, para litas por tipo de ativos esperamos o seguinte parametro `asset` seguido do seu valor `tipo de ativo`.
+
+> Exemplo: `source=traditional`
---
+++ src/pages/tickers.tsx
@@ -0,0 +1,92 @@
+import { useRef, useCallback, useState, ChangeEvent, FormEvent } from 'react'
+import { GetStaticProps } from 'next'
+import ReactMarkdown from 'react-markdown'
+
+import { Pages } from 'layout/pages'
+import { AttributesMarkdown } from 'types/markdown'
+import { TickersPageProps } from 'types/pages'
+
+import Title from 'components/Title'
+import Code from 'components/Code'
+import RunApi from 'components/RunApi'
+
+import Input from 'components/Input'
+import ButtonRun from 'components/ButtonRun'
+
+import { api } from 'services/axios'
+
+type Props = TickersPageProps
+
+const Tickers = ({
+  title,
+  body,
+  versions,
+  patchUrl,
+  dataApi,
+  asset,
+  source
+}: Props) => {
+  const [data, setData] = useState(dataApi)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [query, setQuery] = useState('')
+
+  const handleChangeInput = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target
+      setQuery(value)
+    },
+    []
+  )
+
+  const handleSubmit = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault()
+      const response = await api.get(`${versions}/${patchUrl}?${query}`)
+      setData(response.data)
+    },
+    [versions, patchUrl, query]
+  )
+
+  return (
+    <Pages title={title}>
+      <Title title={title} />
+
+      <ReactMarkdown children={body} />
+
+      <RunApi
+        onSubmit={handleSubmit}
+        method="GET"
+        patchUrl={`${versions}/${patchUrl}`}
+        query={query}
+      >
+        <Input
+          ref={inputRef}
+          name="coins"
+          placeholder={`Query example: ${source} | ${asset}`}
+          isLabel
+          onChange={handleChangeInput}
+        >
+          <ButtonRun type="submit">Run</ButtonRun>
+        </Input>
+      </RunApi>
+
+      <Code code={data} type="Array<Object> | Object" maxHeight="500px" />
+    </Pages>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { attributes, body } = (await import(
+    'content/pages/tickers.md'
+  )) as AttributesMarkdown<TickersPageProps>
+
+  const response = await api.get(
+    `${attributes.versions}/${attributes.patchUrl}`
+  )
+
+  return {
+    props: { ...attributes, body, dataApi: response.data }
+  }
+}
+
+export default Tickers
